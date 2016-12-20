ab17f496a4928c629d4eb54ce7ee2199e4054fc4
Richard Neese <r.neese@gmail.com>
2016-12-20 08:35:31 -0600

    cleaning build scripts for repo

--- setup-repos-build-pkgs.sh
+++ setup-repos-build-pkgs.sh
@@ -2,14 +2,13 @@
 pkg-name=""
 
 repo1="release"
-repo2="head"
+repo2="devel"
 
 apt-get install -y cowbuilder devscripts g++ make cmake libsigc++-2.0-dev libgsm1-dev libpopt-dev libgcrypt11-dev \
-	libspeex-dev libspeexdsp-dev libasound2-dev alsa-utils vorbis-tools sox flac libsox-fmt-mp3 \
-	sqlite3 unzip opus-tools tcl8.6-dev tk-dev alsa-base ntp groff doxygen libopus-dev librtlsdr-dev \
-	git-core uuid-dev qtbase5-dev qttools5-dev-tools qttools5-dev git-core tk8.6-dev ntp flite screen \
-	time inetutils-syslogd vim install-info whiptail dialog logrotate cron usbutils gawk groff watchdog\
-	nginx
+	libspeex-dev libspeexdsp-dev libasound2-dev alsa-utils vorbis-tools sox flac libsox-fmt-mp3 sqlite3 unzip \
+	opus-tools tcl8.6-dev tk-dev alsa-base ntp groff doxygen libopus-dev librtlsdr-dev git-core reprepro nginx \
+	uuid-dev qtbase5-dev qttools5-dev-tools qttools5-dev git-core tk8.6-dev ntp flite screen time inetutils-syslogd \
+	vim install-info whiptail dialog logrotate cron dh-systemd quilt
 
 #Setup repos
 #Stable repo
